40b6138ff3876bfd57faec47087f6febc0b631e7
Jon Klixbüll Langeland <jon@klixbull.org>
2015-01-02 20:02:21 +0100

    First commit

---
+++ Classes/ApiGenerator.php
@@ -0,0 +1,513 @@
+<?php
+
+
+require __DIR__ . '/Bootstrap.php';
+
+class ApiGenerator extends \ApiGenerator\CLI\CLI {
+
+	private $baseTask = NULL;
+
+	private $buildTarget = 'master';
+
+	function __construct($appname = null, $author = null, $copyright = null) {
+		$this->settings = parse_ini_file('../Configuration/api-generator.ini', true);
+		parent::__construct('Kiskstart new TYPO3 FLOW / NEOS project', 'Jon Klixbüll Langeland', '(c) 2014 MOC A/S.');
+	}
+
+	/**
+	 * The main() function gets called if at least one argument is present.
+	 * If no arguments are present, the automatically generated help is displayed.
+	 *
+	 * The main functions job to do the main work of the script.
+	 *
+	 *
+	 * ./snapdev.php create --name=jaboo --source=typo3/flow-base-distribution
+	 * ./snapdev.php create --name=razzel --source=git://git@git.moc.net/Distributions/Aris.git
+	 *
+	 *
+	 */
+	public function main() {
+		if ($this->baseTask != NULL) {
+			call_user_func(array($this, 'execute_' . $this->baseTask));
+		} else {
+			die('Missing argument');
+		}
+	}
+
+	private function execute_build() {
+		$this->outputLine('Building source API', array());
+		try {
+			$this->task_createDirectories();
+			$this->task_prepareSource('git://git.typo3.org/Packages/TYPO3.CMS.git', 'TYPO3.CMS');
+			$this->task_prepareSource('git://git.typo3.org/Flow/Distributions/Base.git', 'TYPO3.FLOW');
+			$this->task_prepareSource('git://git.typo3.org/Neos/Distributions/Base.git', 'TYPO3.NEOS');
+
+			#$this->task_compile('TYPO3.CMS');
+			$this->task_compile('TYPO3.FLOW');
+			$this->task_compile('TYPO3.NEOS');
+
+		} catch (Exception $e) {
+			$this->outputLine('Error: %s', array($e->getMessage()));
+		}
+	}
+
+	/*******************************************************************************************************************
+	 *
+	 ******************************************************************************************************************/
+
+	/**
+	 * Argument is like flag, but just a string.
+	 * ./example.php create --name=jaboo --source="git@github.com:mocdk/vagrant-neosdev.git"
+	 */
+	public function argument_build($opt = null) {
+		if ($opt == 'help') {
+			return 'Build source API';
+		}
+
+		$this->baseTask = 'build';
+	}
+
+	/**
+	 * Argument is like flag, but just a string.
+	 * ./example.php config
+	 */
+	public function argument_config($opt = null) {
+		if ($opt == 'help') {
+			return 'Displays the current configuration';
+		}
+		print_r($this->settings);
+
+		exit();
+	}
+
+	/*******************************************************************************************************************
+	 *
+	 ******************************************************************************************************************/
+
+	private function task_createDirectories() {
+		$this->outputLine('Creating directory structure at: %s', array($this->settings['locations']['base']));
+
+		$storageDir = $this->settings['locations']['base'] . $this->settings['locations']['storage'];
+		$this->outputLine('  Creating directory: %s', array($storageDir));
+		if (!is_dir($storageDir) && !mkdir($storageDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+		$buildDir = $this->settings['locations']['base'] . $this->settings['locations']['build'];
+		$this->outputLine('  Creating directory: %s', array($buildDir));
+		if (!is_dir($buildDir) && !mkdir($buildDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+		$sourceDir = $this->settings['locations']['base'] . $this->settings['locations']['source'];
+		$this->outputLine('  Creating directory: %s', array($sourceDir));
+		if (!is_dir($sourceDir) && !mkdir($sourceDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+		$temporaryDir = $this->settings['locations']['base'] . $this->settings['locations']['temporary'];
+		$this->outputLine('  Creating directory: %s', array($temporaryDir));
+		if (!is_dir($temporaryDir) && !mkdir($temporaryDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+		$logDir = $this->settings['locations']['base'] . $this->settings['locations']['log'];
+		$this->outputLine('  Creating directory: %s', array($logDir));
+		if (!is_dir($logDir) && !mkdir($logDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+		$archiveDir = $this->settings['locations']['base'] . $this->settings['locations']['build'] . 'Archive/';
+		$this->outputLine('  Creating directory: %s', array($archiveDir));
+		if (!is_dir($archiveDir) && !mkdir($archiveDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+		$apiDir = $this->settings['locations']['base'] . $this->settings['locations']['build'] . 'API/';
+		$this->outputLine('  Creating directory: %s', array($apiDir));
+		if (!is_dir($apiDir) && !mkdir($apiDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+		$docsetDir = $this->settings['locations']['base'] . $this->settings['locations']['build'] . 'Docset/';
+		$this->outputLine('  Creating directory: %s', array($docsetDir));
+		if (!is_dir($docsetDir) && !mkdir($docsetDir, 0777)) {
+			$this->outputLine('Failed to create folders...');
+			$this->quit(1);
+		}
+
+	}
+
+	private function task_prepareSource($repository, $alias = NULL) {
+
+		$basename = substr(basename($repository), 0, -4);
+		$repositoryName = (is_null($alias)) ? $basename : $alias;
+		$this->outputLine('Prepearin source %s', array($repositoryName));
+
+		if (!is_dir($this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName)) {
+			$this->outputLine('Local repository does not exists... Clonong..', array());
+
+			$this->helper_systemCall(
+				vsprintf(
+					'git clone %s %s',
+					array(
+						$repository,
+						$repositoryName
+					)
+				),
+				$this->settings['locations']['base'] . $this->settings['locations']['source']
+			);
+
+		} else {
+
+			$this->outputLine('Local repository exists... Resetting..', array());
+			$this->helper_systemCall(
+				vsprintf(
+					'git checkout master',
+					array()
+				),
+				$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName
+			);
+			$this->helper_systemCall(
+				vsprintf(
+					'git pull',
+					array()
+				),
+				$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName
+			);
+		}
+
+		if(is_file($this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName . '/composer.json')){
+			$this->helper_systemCall(
+				vsprintf(
+					'composer install',
+					array()
+				),
+				$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName
+			);
+		}
+	}
+
+	private function task_compile($repositoryName) {
+		$versions = $this->helper_loadVersions($repositoryName);
+
+		foreach ($versions AS $version) {
+
+			$this->task_compileVersion($repositoryName, $version);
+
+		}
+
+	}
+
+	private function task_compileVersion($repositoryName, array $version) {
+		if (in_array($version['name'], $this->settings['skip']['name'])) {
+			$this->outputLine('Skipping %s (%s)',
+				array(
+					$version['name'],
+					$version['version'],
+				));
+			return;
+		}
+
+		$destinationApi = $this->settings['locations']['base'] . $this->settings['locations']['build'] . 'Api/' . $repositoryName . '/' . $this->buildTarget . '/' . $version['name'] . '/';
+
+		if (!is_file($destinationApi . 'commit-' . $version['commit'])) {
+			$this->outputLine('/*******************************************************************************************************************');
+			$this->outputLine(' * Compiling HTML version of %s (%s)', array($version['name'], $version['version']));
+			$this->outputLine(' ******************************************************************************************************************/');
+
+			$this->helper_systemCall(
+				vsprintf(
+					'git checkout --quiet %s',
+					array(
+						$version['commit']
+					)
+				),
+				$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName
+			);
+
+			try {
+				$this->helper_systemCall(
+					vsprintf(
+						'php apigen generate --debug -s %s -d %s --config %s --title "%s"',
+						array(
+							$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName,
+							$destinationApi,
+							$this->settings['locations']['base'] . 'Configuration/apigen_Api.neon',
+							'TYPO3 CMS Version ' . $version['name'] . ' [' . $version['short'] . ']'
+						)
+					),
+					$this->settings['locations']['base'] . 'bin',
+					FALSE,
+					'passthru'
+				);
+				touch($destinationApi . 'commit-' . $version['commit']);
+			} catch (Exception $e) {
+				$this->outputLine('FAIELD compiling %s (%s)',
+					array(
+						$version['name'],
+						$version['version'],
+					));
+
+				$this->helper_systemCall(
+					vsprintf(
+						'rm -rf %s',
+						array(
+							$destinationApi
+						)
+					),
+					$this->settings['locations']['base'] . 'bin'
+				);
+				$this->outputLine('');
+			}
+		} else {
+			$this->outputLine('API Build of %s (%s) exists, continueing',
+				array(
+					$version['name'],
+					$version['version'],
+				));
+		}
+
+		$destinationDocset = $this->settings['locations']['base'] . $this->settings['locations']['build'] . 'Docset/' . $repositoryName . '/' . $this->buildTarget . '/' . $version['name'] . '.docset/';
+		if (!is_file($destinationDocset . 'Contents/Resources/Documents/commit-' . $version['commit'])) {
+			$this->outputLine('/*******************************************************************************************************************');
+			$this->outputLine(' * Compiling DOCSET version of %s (%s)', array($version['name'], $version['version']));
+			$this->outputLine(' ******************************************************************************************************************/');
+
+			$this->helper_systemCall(
+				vsprintf(
+					'git checkout --quiet %s',
+					array(
+						$version['commit']
+					)
+				),
+				$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName
+			);
+/*
+			try {
+				# 1. Create the Docset Folder
+				$this->outputLine('# 1. Create the Docset Folder');
+				$this->helper_systemCall(
+					vsprintf(
+						'mkdir -p %s',
+						array(
+							$destinationDocset . 'Contents/Resources/Documents/'
+						)
+					)
+				);
+
+				$this->outputLine('# 2. Build the HTML Documentation');
+				$this->helper_systemCall(
+					vsprintf(
+						'php apigen generate --debug -s %s -d %s --config %s --title "%s"',
+						array(
+							$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName,
+							$destinationDocset . 'Contents/Resources/Documents/',
+							$this->settings['locations']['base'] . 'Configuration/apigen_Docset.neon',
+							'TYPO3 CMS Version ' . $version['name'] . ' [' . $version['short'] . ']'
+						)
+					),
+					$this->settings['locations']['base'] . 'bin',
+					FALSE,
+					'passthru'
+				);
+
+				$this->outputLine('# 3. Create the Info.plist File');
+				$this->outputLine('# 4. Create the SQLite Index');
+				$this->outputLine('# 5. Adding an Icon');
+				$this->outputLine('# 6. Writing identification file');
+				touch($destinationDocset . 'Contents/Resources/Documents/commit-' . $version['commit']);
+
+				$this->outputLine('# 7. Compressing docset');
+				$this->outputLine('# 8. Create the Feed.xml File');
+
+			} catch (Exception $e) {
+				$this->outputLine('FAIELD compiling %s (%s)',
+					array(
+						$version['name'],
+						$version['version'],
+					));
+
+				$this->helper_systemCall(
+					vsprintf(
+						'rm -rf %s',
+						array(
+							$destinationDocset
+						)
+					),
+					$this->settings['locations']['base'] . 'bin'
+				);
+				$this->outputLine('');
+			}
+			*/
+		} else {
+			$this->outputLine('Build of %s (%s) exists, continueing',
+				array(
+					$version['name'],
+					$version['version'],
+				));
+		}
+
+		$this->outputLine();
+		$this->outputLine();
+
+		return;
+	}
+
+	/*******************************************************************************************************************
+	 *
+	 ******************************************************************************************************************/
+
+	/**
+	 * @param string $repositoryName
+	 * @return array
+	 * @throws Exception
+	 */
+	private function helper_loadVersions($repositoryName) {
+
+		$gitShowRef = $this->helper_systemCall(
+			vsprintf(
+				'git show-ref',
+				array()
+			),
+			$this->settings['locations']['base'] . $this->settings['locations']['source'] . $repositoryName
+		);
+
+		$this->outputLine('Extracting tags for target: %s on %s', array($this->buildTarget, $repositoryName));
+
+		switch ($this->buildTarget) {
+			case 'stable':
+				$pattern = '/(\S*)\srefs\/tags\/TYPO3_([0-9]*)-([0-9]*)-([0-9]*)$/';
+				$versions = array();
+				foreach ($gitShowRef AS $tag) {
+					if (preg_match($pattern, $tag, $match)) {
+						$versions[] = array(
+							'commit' => $match[1],
+							'short' => substr($match[1], 0, 7),
+							'version' => $match[2] . '.' . $match[3] . '.' . $match[4],
+							'branch' => $match[2] . '.' . $match[3],
+							'name' => $match[2] . '.' . $match[3] . '.' . $match[4]
+						);
+					} else {
+						continue;
+					}
+				}
+				break;
+			case 'latest':
+				$pattern = '/(\S*)\srefs\/tags\/TYPO3_([0-9]*)-([0-9]*)-([0-9]*)$/';
+				$temp = array();
+				foreach ($gitShowRef AS $tag) {
+					if (preg_match($pattern, $tag, $match)) {
+						if ($temp[$match[2]][$match[3]]['latest'] < $match[4]) {
+							$temp[$match[2]][$match[3]]['latest'] = $match[4];
+							$temp[$match[2]][$match[3]][$match[4]] = array(
+								'commit' => $match[1],
+								'short' => substr($match[1], 0, 7),
+								'version' => $match[2] . '.' . $match[3] . '.' . $match[4],
+								'branch' => $match[2] . '.' . $match[3],
+								'name' => $match[2] . '.' . $match[3] . 'latest'
+							);
+						}
+					} else {
+						continue;
+					}
+				}
+
+				$versions = array();
+				foreach ($temp AS $major) {
+					foreach ($major AS $minor) {
+						$versions[] = $minor[$minor['latest']];
+					}
+				}
+				break;
+			case 'dev':
+				$pattern = '/(\S*)\srefs\/remotes\/origin\/TYPO3_([0-9]*)-([0-9]*)/';
+				$versions = array();
+				foreach ($gitShowRef AS $tag) {
+					if (preg_match($pattern, $tag, $match)) {
+						$versions[] = array(
+							'commit' => $match[1],
+							'short' => substr($match[1], 0, 7),
+							'version' => $match[2] . '.' . $match[3] . '.0-dev',
+							'branch' => $match[2] . '.' . $match[3],
+							'name' => $match[2] . '.' . $match[3] . 'dev'
+						);
+					} else {
+						continue;
+					}
+				}
+				break;
+			case 'master':
+				$pattern = '/(\S*)\srefs\/remotes\/origin\/master/';
+				$versions = array();
+				foreach ($gitShowRef AS $tag) {
+					if (preg_match($pattern, $tag, $match)) {
+						$versions[] = array(
+							'commit' => $match[1],
+							'short' => substr($match[1], 0, 7),
+							'version' => 'master',
+							'name' => 'master'
+						);
+					} else {
+						continue;
+					}
+				}
+				break;
+			default:
+				throw new Exception('Unknown build parameter. [ stable | latest | dev | master ]', $e);
+		}
+
+		return $versions;
+	}
+
+	private function helper_systemCall($command, $changeWorkingDirectory = NULL, $dryRun = FALSE, $use = 'exec') {
+		$output = '';
+		if ($changeWorkingDirectory != NULL) {
+			$currentWorkingDirectory = getcwd();
+			chdir($changeWorkingDirectory);
+		}
+		$this->outputLine('Running: > %s', array($command));
+		if (!$dryRun) {
+
+			switch ($use) {
+				case 'exec':
+					exec($command, $output, $return_var);
+					if ($return_var != 0) {
+						throw new \Exception('Command faild..');
+					}
+					break;
+				case 'passthru':
+					passthru($command, $return_var);
+					if ($return_var != 0) {
+						throw new \Exception('Command faild..');
+					}
+					break;
+				case 'system':
+					die('Not implemented');
+					break;
+				default:
+					die('Unknowen function');
+			}
+
+		}
+		if ($changeWorkingDirectory != NULL) {
+			chdir($currentWorkingDirectory);
+		}
+
+		return $output;
+	}
+
+}
+
+/**
+ *IMPORTANT, instantiate your class! i.e. new Classname();
+ */
+new ApiGenerator();
+
---
+++ Classes/Cli/Cli.php
@@ -0,0 +1,417 @@
+<?php
+
+namespace ApiGenerator\Cli;
+
+/**
+ * Command line interface
+ * License: MIT
+ * Allows easily writing OOP CLI scripts in PHP
+ *
+ * @author sami
+ *
+ */
+class CLI {
+
+	# first define colors to use
+	private $_colors = array(
+		"LIGHT_RED" => "[1;31m",
+		"LIGHT_GREEN" => "[1;32m",
+		"YELLOW" => "[1;33m",
+		"LIGHT_BLUE" => "[1;34m",
+		"MAGENTA" => "[1;35m",
+		"LIGHT_CYAN" => "[1;36m",
+		"WHITE" => "[1;37m",
+		"NORMAL" => "[0m",
+		"BLACK" => "[0;30m",
+		"RED" => "[0;31m",
+		"GREEN" => "[0;32m",
+		"BROWN" => "[0;33m",
+		"BLUE" => "[0;34m",
+		"CYAN" => "[0;36m",
+		"BOLD" => "[1m",
+		"UNDERSCORE" => "[4m",
+		"REVERSE" => "[7m",
+	);
+
+	public $stdin = NULL;
+
+	private $appname = 'CLI Framework';
+
+	private $author = 'Sami Khan';
+
+	private $copyright = '(c) 2011 Sami Khan';
+
+	function __construct($appname = null, $author = null, $copyright = null) {
+		if ($appname) {
+			$this->appname = $appname;
+		}
+		if ($author) {
+			$this->author = $author;
+		}
+		if ($copyright) {
+			$this->copyright = $copyright;
+		}
+
+		if ($this::isCli()) {
+			$args = $this::parseArgs();
+
+			foreach ($args['flags'] as $flag) {
+				$method_name = 'flag_' . $flag;
+				if (method_exists($this, $method_name)) {
+					call_user_func(array($this, $method_name));
+				}
+			}
+
+			foreach ($args['arguments'] as $arg) {
+				$method_name = 'argument_' . $arg;
+				if (method_exists($this, $method_name)) {
+					call_user_func(array($this, $method_name));
+				}
+			}
+
+			foreach ($args['options'] as $arg) {
+				if (is_string($arg) === false && isset($arg[0]) && isset($arg[1])) {
+					$method_name = 'option_' . $arg[0];
+					if (method_exists($this, $method_name)) {
+						call_user_func(array($this, $method_name), $arg[1]);
+					}
+				} else {
+					$method_name = 'option_' . $arg;
+					if (method_exists($this, $method_name)) {
+						call_user_func(array($this, $method_name));
+					}
+				}
+			}
+			global $argv;
+			if (count($argv) === 1) {
+				$this->help();
+			} else {
+				$this->main();
+			}
+			exit();
+		}
+	}
+
+	/**
+	 * Set the name of the app
+	 */
+	public function setAppName($appname) {
+		$this->appname = $appname;
+	}
+
+	/**
+	 * Simply test whether or not we are running in CLI mode.
+	 */
+	public static function isCli() {
+		if (!defined('STDIN') && self::isCgi()) {
+			if (getenv('TERM')) {
+				return true;
+			}
+			return false;
+		}
+		return defined('STDIN');
+	}
+
+	/**
+	 *
+	 * Example input: ./script.php -a arg1 --opt1 arg2 -bcde --opt2=val2 arg3 arg4 arg5 -fg --opt3
+	 * Example output:
+	 * Array
+	 * (
+	 * [exec] => ./script.php
+	 * [options] => Array
+	 * (
+	 * [0] => opt1
+	 * [1] => Array
+	 * (
+	 * [0] => opt2
+	 * [1] => val2
+	 * )
+	 * [2] => opt3
+	 * )
+	 * [flags] => Array
+	 * (
+	 * [0] => a
+	 * [1] => b
+	 * [2] => c
+	 * [3] => d
+	 * [4] => e
+	 * [5] => f
+	 * [6] => g
+	 * )
+	 * [arguments] => Array
+	 * (
+	 * [0] => arg1
+	 * [1] => arg2
+	 * [2] => arg3
+	 * [3] => arg4
+	 * [4] => arg5
+	 * )
+	 * )
+	 */
+	public static function parseArgs($args = null) {
+		if ($args == null) {
+			global $argv, $argc;
+			$args = $argv;
+		}
+		$ret = array(
+			'exec' => '',
+			'options' => array(),
+			'flags' => array(),
+			'arguments' => array(),
+		);
+		if (count($args) == 0) {
+			return $ret;
+		}
+
+		$ret['exec'] = array_shift($args);
+
+		while (($arg = array_shift($args)) != NULL) {
+			// Is it a option? (prefixed with --)
+			if (substr($arg, 0, 2) === '--') {
+				$option = substr($arg, 2);
+
+				// is it the syntax '--option=argument'?
+				if (strpos($option, '=') !== FALSE) {
+					array_push($ret['options'], explode('=', $option, 2));
+				} else {
+					array_push($ret['options'], $option);
+				}
+
+				continue;
+			}
+
+			// Is it a flag or a serial of flags? (prefixed with -)
+			if (substr($arg, 0, 1) === '-') {
+				for ($i = 1; isset($arg[$i]); $i++) {
+					$ret['flags'][] = $arg[$i];
+				}
+
+				continue;
+			}
+
+			// finally, it is not option, nor flag
+			$ret['arguments'][] = $arg;
+			continue;
+		}
+		return $ret;
+	}
+
+	/**
+	 * ./script.php arg3
+	 * input : arg3 ; return true
+	 * input : arg4 ; return false
+	 *
+	 * @param unknown_type $argument
+	 * @return bool
+	 */
+	function getArg($arg) {
+		$args = $this::parseArgs();
+		if (in_array($arg, $args)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if a certain option is set and returns the string.
+	 * ./script --opt1
+	 *
+	 * @param unknown_type $option
+	 */
+	function getOption($option) {
+		//$args = $this->parseArgs();
+		//return $args['options'][$option];
+	}
+
+	/**
+	 * Handle the default help flag
+	 */
+	private function flag_h($opt = null) {
+		if ($opt == 'help') {
+			return 'Display help.';
+		}
+		$this->help();
+	}
+
+	/**
+	 * Handle the default help argument
+	 */
+	private function argument_help($opt = null) {
+		if ($opt == 'help') {
+			return 'Display help.';
+		}
+		$this->help();
+	}
+
+	/**
+	 * ./script.php --option1
+	 *  --option1=var1 =>
+	 *    array('options' =>
+	 *      array( 0 => 'option1',
+	 *        array('0' => 'option1', '1' => 'var1'))
+	 *
+	 * @param unknown_type $opt
+	 */
+	private function option_help($opt = null) {
+
+		if ($opt == 'help') {
+			return 'Display help for a specific command. ?=command';
+		}
+
+		if (substr($opt, 0, 2) == '--') {
+			$opt = str_replace('--', '', $opt);
+			//option
+			$method = 'option_' . $opt;
+
+		} elseif (substr($opt, 0, 1) == '-') {
+
+			//flag
+			$opt = str_replace('-', '', $opt);
+			$method = 'flag_' . $opt;
+
+		} else {
+			//argument
+			$method = 'argument_' . $opt;
+		}
+
+		if (method_exists($this, $method)) {
+			print "\n" . $this->$method('help') . "\n\n";
+		} elseif ($opt == null) {
+			$this->help();
+		} else {
+			print "\n" . 'Option, argument or flag not found.' . "\n\n";
+		}
+
+	}
+
+	/**
+	 * Print out help for this program.
+	 * The help is auto generated using various variables.
+	 */
+	public function help($args = array()) {
+		print $this->colorText($this->appname, "LIGHT_RED") . "\n";
+		print $this->colorText($this->author . ' - ' . $this->copyright, "LIGHT_RED") . "\n";
+
+		for ($i = 0; $i < strlen($this->appname . $this->author . $this->copyright); $i++) {
+			print '-';
+		}
+		print "\n";
+
+		$methods = get_class_methods(get_class($this));
+		foreach ($methods as $method) {
+			if (substr($method, 0, 4) == 'flag') {
+				$flag = str_replace('flag_', '', $method);
+				$flags_help[$flag] = $this->$method('help');
+			} elseif (substr($method, 0, 8) == 'argument') {
+				$argument = str_replace('argument_', '', $method);
+				$arguments_help[$argument] = $this->$method('help');
+			} elseif (substr($method, 0, 6) == 'option') {
+				$option = str_replace('option_', '', $method);
+				$options_help[$option] = $this->$method('help');
+			}
+		}
+
+		print $this->colorText(' Flags:', "BLUE") . "\n";
+		foreach ($flags_help as $flag => $desc) {
+			$spaces = 20 - strlen($flag);
+			printf("  -%s%" . $spaces . "s%s\n", $flag, '', $desc);
+		}
+		print "\n" . $this->colorText(' Arguments:', "BLUE") . "\n";
+		foreach ($arguments_help as $arg => $desc) {
+			$spaces = 20 - strlen($arg) + 1;
+
+			printf("  %s%" . $spaces . "s%s\n", $arg, '', $desc);
+		}
+		print "\n" . $this->colorText(' Options:', "BLUE") . "\n";
+		foreach ($options_help as $opt => $desc) {
+			$spaces = 20 - strlen($opt) - strlen($opt) - 4;
+			printf("  --%s;%s=?%" . $spaces . "s%s\n", $opt, $opt, '', $desc);
+		}
+		print "\n";
+		exit();
+	}
+
+	/**
+	 * Output coloized text to the terminal
+	 */
+	public function colorText($text, $color = "NORMAL", $back = 1) {
+		$out = $this->_colors[$color];
+		if ($out == "") {
+			$out = "[0m";
+		}
+		if ($back) {
+			return chr(27) . "$out$text" . chr(27) . "[0m"; #.chr(27);
+		} else {
+			echo chr(27) . "$out$text" . chr(27) . chr(27) . "[0m"; #.chr(27);
+		}
+	}
+
+	public function output($text, array $arguments = array(), $force = FALSE) {
+		if ($arguments !== array()) {
+			$text = vsprintf($text, $arguments);
+		}
+
+		echo $text;
+		return TRUE;
+		/*
+		if (isset($this->cli_args['-ss'])) {
+
+		} elseif (isset($this->cli_args['-s']) || isset($this->cli_args['--silent'])) {
+			if ($force) {
+				echo $text;
+				return TRUE;
+			}
+		} else {
+			echo $text;
+			return TRUE;
+		}
+		return FALSE;
+		*/
+	}
+
+	public function outputLine($text = '', array $arguments = array()) {
+		return $this->output($text . PHP_EOL, $arguments);
+	}
+
+	public function quit($exitCode = 0) {
+		die($exitCode);
+	}
+
+	public function getInput($question = "Are you sure you want to do this?  Type 'yes' to continue:") {
+		echo $question . " ";
+		$handle = fopen("php://stdin", "r");
+		$line = fgets($handle);
+		$answer = trim($line);
+		return $answer;
+	}
+
+	public function keyboardInput() {
+		// Have to open the stdin stream only ONCE! otherwise I cannot read multiple lines from it... :
+		if (!$this->stdin) {
+			$this->stdin = fopen('php://stdin', 'r');
+		}
+		while (FALSE == ($line = fgets($this->stdin, 1000))) {
+
+		}
+		return trim($line);
+	}
+
+	public function keyboardInput_yes($msg = '') {
+		// ONLY makes sense to echo it out since we are awaiting keyboard input - that cannot be silenced
+		echo $msg . ' (Yes/No + return): ';
+		return in_array(strtolower($this->cli_keyboardInput()), array('y', 'yes'));
+	}
+
+	public function indent($str, $indent) {
+		$lines = explode(LF, wordwrap($str, 75 - $indent));
+		$indentStr = str_pad('', $indent, ' ');
+		foreach ($lines as $k => $v) {
+			$lines[$k] = $indentStr . $lines[$k];
+		}
+		return implode(LF, $lines);
+	}
+
+}
+
